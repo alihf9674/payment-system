f0ad90f29e92072b2852573b605df465928e5edd
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-11-06 18:37:44 +0330

    Add product

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -9,19 +9,19 @@
 	</div>
 	<div class="card-body">
 		<div class="row mb-5">
-
-				<div class="col-md-4 mb-5 ">
-				<div class="card" style="width: 18rem;">
-				<img class="card-img-top" src="" alt="Card image cap">
-					<div class="card-body">
-					<h5 class="card-title"></h5>
-						<p class="card-text">  </p>
-					<a href="" class="btn btn-primary">@lang('payment.add to basket')</a>
-					</div>
-				</div>
-			</div>
-
-		</div>
+            @foreach ($products as $product)
+                <div class="col-md-4 mb-5 ">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{$product->image}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$product->title}}</h5>
+                            <p class="card-text"> {{$product->description}} </p>
+                            <a href="" class="btn btn-primary">@lang('payment.add to basket')</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
 	</div>
 </div>
 
